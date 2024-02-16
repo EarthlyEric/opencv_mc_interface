cd255044c0948bf697e3c86573f655798e4ab69a
EarthlyEric <earthlyeric6@gmail.com>
2024-02-17 00:38:27 +0800

    add a simple ui for regular user use.

--- ui.py
+++ ui.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+from main import boot
+
+def power_on_action():
+    
+    
+def exit_action():
+    app.destroy()
 
 app = tk.Tk()
 width = 300
@@ -6,7 +13,16 @@ height = 300
 left = int(float(app.winfo_screenheight())/2)
 top = int(float(app.winfo_width())/2)
 app.geometry(f'{width}x{height}+{left}+{top}')
-app.winfo_name="OpenCV Minecraft Interface Launcher"
 
+app.title("OpenCV MC Interface")
+
+widget_bg = tk.Frame(app,bg="light gray")
+widget_bg.pack(expand=True)
+
+startup = tk.Button(widget_bg, text="POWER ON CAMERA")
+startup.pack(side="left")
+
+exit = tk.Button(widget_bg, text="EXIT",command=exit_action)
+exit.pack(side="left",padx=10)
 
 app.mainloop()
